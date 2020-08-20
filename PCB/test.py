1850c81dac67d71c9b07943fe68969efc2babb41
dl <571435345@qq.com>
2020-08-20 16:03:58 +0800

    disable crop&eraseing for PCB

--- PCB/test.py
+++ PCB/test.py
@@ -24,7 +24,7 @@ except ImportError:  # will be 3.x series
 
 parser = argparse.ArgumentParser(description='Testing')
 parser.add_argument('--gpu_ids', default='0', type=str, help='gpu_ids: e.g. 0  0,1,2  0,2')
-parser.add_argument('--which_epoch', default='last', type=str, help='0,1,2,3...or last')
+parser.add_argument('--which_epoch', default='best', type=str, help='0,1,2,3...or last')
 parser.add_argument('--test_dir', default='duke', type=str, help='./test_data')
 parser.add_argument('--name', default='pcb', type=str, help='save model path')
 parser.add_argument('--batchsize', default=64, type=int, help='batchsize')
